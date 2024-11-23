f0f0780a24b036079e9914bcfe7709323dcac2b2
TAHUID679 <bot09673@gmail.com>
2024-11-23 20:59:02 +0600

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -12,7 +12,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
-  const permission = ["100092747803433"];
+  const permission = ["100076339585458"];
    if (!permission.includes(event.senderID))
    return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
